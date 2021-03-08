14b8e421c86c1bdf6998972469f64199a3e68572
Владислав Капустин <70846638+vladhanzo@users.noreply.github.com>
2021-03-08 12:32:54 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr1 = [];
+    let arr2 = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arr1[i] = bracketsConfig[i][0];
+        arr2[i] = bracketsConfig[i][1];
+    }
+    let arr = str.split("");
+    for (let i = 0; i < arr.length; i++) {
+        for (let index in arr1) {
+            if (arr[i] == arr1[index]) {
+                if (arr[i + 1] == arr2[index]) {
+                    arr.splice(i, 2);
+                    i = -1;
+                    break;
+                }
+            }
+        }
+    }
+    if (arr.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
